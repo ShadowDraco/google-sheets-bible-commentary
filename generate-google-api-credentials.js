20c6fba9aa2c96f21b0e83e3b84cb7141855f1ee
Ethan Storm <stormethan10@gmail.com>
2023-01-24 03:46:51 -0800

    more cleanup for deployment

--- generate-google-api-credentials.js
+++ generate-google-api-credentials.js
@@ -4,7 +4,7 @@ require("dotenv").config()
 
 const credentials = {
   type: "service_account",
-  project_id: "my-project",
+  project_id: process.env.GOOGLE_PROJECT_ID,
   private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
   private_key: process.env.GOOGLE_PRIVATE_KEY,
   client_email: process.env.GOOGLE_CLIENT_EMAIL,
